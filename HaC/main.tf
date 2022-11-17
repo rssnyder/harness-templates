5e5b0b7f1565f1f950219d196f17eca0b89801c1
Riley Snyder <riley.snyder@harness.io>
2022-11-17 13:09:30 -0600

    add default env/infra

--- HaC/main.tf
+++ HaC/main.tf
@@ -7,3 +7,55 @@ resource "harness_platform_organization" "this" {
     "git:rssnyder/harness-templates/HaC"
   ]
 }
+
+resource "harness_platform_project" "default" {
+  identifier = "default"
+  name       = "default"
+  org_id     = harness_platform_organization.this.id
+}
+
+resource "harness_platform_environment" "dev" {
+  identifier = "dev"
+  name       = "dev"
+  org_id     = harness_platform_organization.this.id
+  project_id = harness_platform_project.default.id
+  type       = "PreProduction"
+  yaml       = <<EOF
+environment:
+  name: dev
+  identifier: dev
+  description: ""
+  tags: {}
+  type: PreProduction
+  orgIdentifier: default
+  projectIdentifier: default
+  variables: []
+EOF
+}
+
+resource "harness_platform_infrastructure" "dev_sa" {
+  identifier      = "sa"
+  name            = "sa"
+  org_id          = harness_platform_organization.this.id
+  project_id      = harness_platform_project.default.id
+  env_id          = harness_platform_environment.dev.id
+  type            = "KubernetesDirect"
+  deployment_type = "Kubernetes"
+  yaml            = <<EOF
+infrastructureDefinition:
+  name: sa
+  identifier: sa
+  description: ""
+  tags: {}
+  orgIdentifier: default
+  projectIdentifier: default
+  environmentRef: dev
+  deploymentType: Kubernetes
+  type: KubernetesDirect
+  spec:
+    connectorRef: account.sagcp
+    namespace: riley-dev-<+service.name>
+    releaseName: release-<+INFRA_KEY>
+  allowSimultaneousDeployments: false
+EOF
+}
